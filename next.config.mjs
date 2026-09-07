d0962ac2f57a44b2bc95c9b8203656c85d3fc362
Charles Akinnurun <charlesakinnurun@gmail.com>
2026-09-07 12:12:47 +0100

    ADD: next config

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["res.cloudinary.com","cdn.worldvectorlogo.com","encrypted-tbn0.gstatic.com","streak-stats.demolab.com","ghchart.rshah.org"], // Add your image hosting domains here
+    },
+  };
+  
+  export default nextConfig;
+  
