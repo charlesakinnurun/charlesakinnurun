5f3effaaa639c78bbd9f7cb95675964bb0421f74
Charles Akinnurun <charlesakinnurun@gmail.com>
2026-09-22 23:30:26 +0100

    feat(home): add experience and research sections

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
+import Experience from "@/components/Experience";
 import OpenSource from "@/components/OpenSource";
 import Education from "@/components/Education";
 import Certifications from "@/components/Certifications";
+import Research from "@/components/Research";
 import Receipts from "@/components/Receipts";
 import Programming from "@/components/Programming";
 import ContactForm from "@/components/ContactForm";
@@ -12,11 +14,13 @@ export default function Home() {
   return (
    <div className = "bg-[#151312]">
    <Hero/>
-   <Education/>
-   <Receipts/>
    <Projects/>
+   <Experience/>
    <OpenSource/>
    <Certifications/>
+   <Receipts/>
+   <Education/>
+   <Research/>
    {/* <Programming/> */}
    <ContactForm/>
    
